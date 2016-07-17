97ac3a8732c925b44870bb40c7b095e97abb14d7
Yuri Chupyrkin <Yuri Chupyrkin>
2016-07-17 23:12:14 +0300

    added task3

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Task3Logic;
+
+namespace Task3ConsoleUI
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var test = new WordCounter();
+      var firstTest = test.Counter(@"D:\Training EPAM Nastya\Tasks\Day9\book2.txt");
+
+      foreach (var pair in firstTest.OrderByDescending(r => r.Value).Take(1000))
+      {
+        Console.WriteLine("{0}______{1}", pair.Key, pair.Value);
+      }
+      Console.WriteLine(firstTest.Count());
+    }
+  }
+}
---
+++ Task3Logic/WordCounter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Task3Logic
+{
+  /// <summary>
+  /// class for count words in text
+  /// </summary>
+  public class WordCounter
+  {
+    /// <summary>
+    /// method Counter
+    /// </summary>
+    /// <param name="filePath">Path to text file</param>
+    /// <returns>method returns Dictionary<string, int></returns>
+    public Dictionary<string, int> Counter(string filePath)
+    {
+      if (string.IsNullOrEmpty(filePath))
+      {
+        return new Dictionary<string, int>();
+      }
+
+      string text = File.ReadAllText(filePath);
+
+      var splittedText = text.Split(' ', ',', '.', '!', '?', '\r', '\n', ':', ';');
+      var dictionary = new Dictionary<string, int>();
+
+      for (int i = 0; i < splittedText.Length; i++)
+      {
+        if (splittedText[i] == "")
+        {
+          continue;
+        }
+
+        string currentWord = splittedText[i].ToLower();
+ 
+        if (dictionary.ContainsKey(currentWord))
+        {
+          dictionary[currentWord] = dictionary[currentWord] + 1;
+        }
+        else
+        {
+          dictionary.Add(currentWord, 1);
+        }
+      }
+
+      return dictionary;
+    }
+  }
+}
